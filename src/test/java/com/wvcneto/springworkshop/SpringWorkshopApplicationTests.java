4d967299215c5bc7dbaef20cbc15eab47b64cb0b
wvcneto <wvcneto@gmail.com>
2020-11-05 22:15:41 -0300

    project-created

---
+++ src/test/java/com/wvcneto/springworkshop/SpringWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wvcneto.springworkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
