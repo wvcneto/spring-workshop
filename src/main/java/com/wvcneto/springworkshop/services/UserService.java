0e405ee1c880cf7892ea597c29e2ee2440a5ca9d
wvcneto <wvcneto@gmail.com>
2020-11-06 22:52:22 -0300

    services and component registration

---
+++ src/main/java/com/wvcneto/springworkshop/services/UserService.java
@@ -0,0 +1,26 @@
+package com.wvcneto.springworkshop.services;
+
+import com.wvcneto.springworkshop.entities.User;
+import com.wvcneto.springworkshop.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> user =  userRepository.findById(id);
+
+        return user.get();
+    }
+}
