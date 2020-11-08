ac9b4c6a9559f12574992f797d10097322767bb2
wvcneto <wvcneto@gmail.com>
2020-11-08 15:40:49 -0300

    update-exception-handler

--- src/main/java/com/wvcneto/springworkshop/services/UserService.java
+++ src/main/java/com/wvcneto/springworkshop/services/UserService.java
@@ -9,6 +9,7 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.Optional;
 
@@ -43,11 +44,13 @@ public class UserService {
     }
 
     public User update(Long id, User newData) {
-        User entity = userRepository.getOne(id);
-
-        updateData(entity, newData);
-
-        return userRepository.save(entity);
+        try {
+            User entity = userRepository.getOne(id);
+            updateData(entity, newData);
+            return userRepository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User newData) {
