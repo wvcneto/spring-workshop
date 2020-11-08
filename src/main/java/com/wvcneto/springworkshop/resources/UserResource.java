2a789d0b672146d00f9b398c5d5b8301f4d3adb8
wvcneto <wvcneto@gmail.com>
2020-11-07 22:42:43 -0300

    create-user

--- src/main/java/com/wvcneto/springworkshop/resources/UserResource.java
+++ src/main/java/com/wvcneto/springworkshop/resources/UserResource.java
@@ -4,11 +4,10 @@ import com.wvcneto.springworkshop.entities.User;
 import com.wvcneto.springworkshop.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -30,5 +29,12 @@ public class UserResource {
         User user = userService.findById(id);
 
         return ResponseEntity.ok().body(user);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> create(@RequestBody User user) {
+        user = userService.create(user);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+        return ResponseEntity.created(uri).body(user);
     }
 }
